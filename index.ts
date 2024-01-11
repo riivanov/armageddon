192ffedf0dafb25ef3d5fb8548ad04d0907efe80
Radoslav Ivanov <riivanov63@gmail.com>
2024-01-11 17:17:52 +0200

    add express

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("works");
+import express from "express";
+const app = express();
+const port = 7890;
+
+app.get("/", (req, res) => {
+  res.send(`hello world`);
+});
+
+app.listen(port, () => {
+  console.log(`[server]: server is running at http://localhost:${port}`);
+});
