9ea11a4bed11424080be6ae39e02e6f2527e106e
Radoslav Ivanov <riivanov63@gmail.com>
2024-01-15 17:05:49 +0200

    use real POST request values; not mockRequest

--- src/index.ts
+++ src/index.ts
@@ -13,15 +13,7 @@ app.use(express.json());
 dotenv.config();
 const port = process.env.PORT;
 
-const mockReq = {
-  dateStart: "2019-01-01",
-  dateEnd: "2019-01-07",
-  within: {
-    value: 9000000,
-    units: "kilometers",
-  },
-};
-
+//  https://api.nasa.gov/neo/rest/v1/feed?start_date=START_DATE&end_date=END_DATE&api_key=API_KEY
 app.post(
   `/`,
   tryCatch(async (req: any, res: any) => {
@@ -30,9 +22,16 @@ app.post(
     if (!body) throw new Error(`POST request without a body`);
 
     const { dateStart, dateEnd } = body;
+    const {
+      within: { value: maxAsteroidRange },
+    } = body;
     // if no start/end
     if (!dateStart) throw new Error(`dateStart is a required field`);
     if (!dateEnd) throw new Error(`dateEnd is a required field`);
+    if (!maxAsteroidRange || maxAsteroidRange < 0)
+      throw new Error(`within->value must be > 0`);
+
+    console.log(maxAsteroidRange);
 
     const start = DateTime.fromFormat(dateStart, `yyyy-MM-dd`);
     const end = DateTime.fromFormat(dateEnd, `yyyy-MM-dd`);
@@ -42,37 +41,34 @@ app.post(
     if (!end.isValid)
       throw new Error(`End Date Invalid ${end.invalidExplanation}`);
 
-    res.json(body);
+    const url = new URL(process.env.NASA_API_URL!);
+    const queryParams = {
+      api_key: process.env.NASA_API_KEY,
+      start_date: start.toISODate(),
+      end_date: end.toISODate(),
+    };
+    url.search = querystring.encode(queryParams);
+
+    const { data } = await axios.get<NASAResponse>(url.href);
+    const near_earth_objects = data.near_earth_objects as NEO;
+
+    const keys = Object.keys(near_earth_objects).sort();
+
+    let dangerous: Asteroid[] = [];
+    for (const day of keys) {
+      dangerous.push(
+        ...near_earth_objects[day].filter((asteroid) => {
+          const miss_distance =
+            asteroid.close_approach_data[0].miss_distance.kilometers;
+
+          return Number(miss_distance) <= maxAsteroidRange;
+        })
+      );
+    }
+
+    res.json(dangerous);
   })
 );
-
-//  https://api.nasa.gov/neo/rest/v1/feed?start_date=START_DATE&end_date=END_DATE&api_key=API_KEY
-app.get("/", async (req, res) => {
-  const url = new URL(process.env.NASA_API_URL!);
-  const queryParams = {
-    api_key: process.env.NASA_API_KEY,
-  };
-  url.search = querystring.encode(queryParams);
-
-  const { data } = await axios.get<NASAResponse>(url.href);
-  const near_earth_objects = data.near_earth_objects as NEO;
-
-  const keys = Object.keys(near_earth_objects).sort();
-
-  let dangerous: Asteroid[] = [];
-  for (const day of keys) {
-    dangerous.push(
-      ...near_earth_objects[day].filter((asteroid) => {
-        const miss_distance =
-          asteroid.close_approach_data[0].miss_distance.kilometers;
-
-        return Number(miss_distance) <= mockReq.within.value;
-      })
-    );
-  }
-
-  res.send(dangerous);
-});
 
 app.use(errorHandler);
 
